b3c260de87847458280669ada5e02ffee2846717
Siriwimol17 <marr.ap1703@gmail.com>
2024-10-17 21:00:37 +0700

    login popup

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -8,6 +8,11 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent implements OnInit {
   isLoggedIn: boolean = false;
+  // Properties for login popup
+  username: string = '';
+  password: string = '';
+  showPassword: boolean = false;
+  showLoginPopup: boolean = false;
 
   constructor(private router: Router) {}
 
@@ -21,5 +26,26 @@ export class NavbarComponent implements OnInit {
     localStorage.removeItem('token'); // Remove token on logout
     this.isLoggedIn = false;
     this.router.navigate(['#']); // Redirect to login page
+  }
+  // showLoginPopup = false; // ตัวแปรนี้จะใช้สำหรับควบคุมการแสดง popup
+
+  // Toggle password visibility
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  // ฟังก์ชันสำหรับแสดง popup
+  openLoginPopup() {
+    this.showLoginPopup = true;
+  }
+
+  // ฟังก์ชันสำหรับปิด popup
+  closeLoginPopup() {
+    this.showLoginPopup = false;
+  }
+
+  login() {
+    // เพิ่มโค้ดการล็อกอินของคุณที่นี่
+    this.closeLoginPopup(); // ปิด popup เมื่อเข้าสู่ระบบสำเร็จ
   }
 }
